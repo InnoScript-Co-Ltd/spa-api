989f87a873725b5487a1c0db762a29b19053a56f
ShinKhantXoX <shinkhantlaybal@gmail.com>
2025-01-09 22:36:41 +0630

    add auth sanctum

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -2,18 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Dashboard\{
+    AuthController,
     UserController,
 };
 
 
 Route::prefix('v1')->group(function () {
 
-    Route::controller(UserController::class)->prefix('users')->group(function () {
-        Route::get('/', 'index');
-        Route::post('/', 'store');
-        Route::get('/{id}', 'show');
-        Route::put('/{id}', 'update');
-        Route::delete('/{id}', 'destroy');
+    Route::post('/auth/login', [AuthController::class, 'login'])->name('login');
+
+    // Protect routes with Sanctum authentication middleware
+    Route::middleware('auth:sanctum')->group(function () {
+        // Route to get the authenticated user's information
+
+        // UserController routes under 'users' prefix
+        Route::controller(UserController::class)->prefix('users')->group(function () {
+            Route::get('/', 'index');          // List all users
+            Route::post('/', 'store');        // Create a new user
+            Route::get('/{id}', 'show');      // Get a specific user
+            Route::put('/{id}', 'update');    // Update a user
+            Route::delete('/{id}', 'destroy');// Delete a user
+        });
     });
 
 });
