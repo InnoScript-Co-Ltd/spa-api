7126fc8956dd27f9724e95a5f83afbe84743650b
ShinKhantXoX <shinkhantlaybal@gmail.com>
2025-02-15 11:05:49 +0630

    feat employee controller

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,14 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'username' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'email_confirm_at' => now(),
+            'phone' => "999382932",
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role' => "Admin",
+            'status' => "Active"
         ];
     }
 
